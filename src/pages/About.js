758e3e48f345d0ee017e4f69595027f7ac08ad7d
Paveluts42 <55815780+Paveluts42@users.noreply.github.com>
2019-12-31 03:05:59 +0300

    create

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const About = () => {
+    return (
+        <div className="jumbotron ">
+            <div className="container">
+                <h1 className="display-4">Info</h1>
+                <p className="lead">V App <strong>1.0.3</strong></p>
+            </div>
+        </div>
+    )
+
+
+}
